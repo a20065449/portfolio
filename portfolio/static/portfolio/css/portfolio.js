d74fa5b4159ea9e9d131b1e64b4614a436a84e6a
a20065449 <a20065449@alunos.ulht.pt>
2022-06-10 11:02:47 +0100

    temperatura

--- portfolio/static/portfolio/css/portfolio.js
+++ portfolio/static/portfolio/css/portfolio.js
@@ -1,3 +1,5 @@
+
+/*
 document.addEventListener('DOMContentLoaded', function() {
     fetch('http://api.exchangeratesapi.io/v1/latest?access_key=feeea4a36c99c7c5f31c70dbfae3d215')// envia GET request ao URL
         .then(response => response.json()) // Põe a responsta no formato json
@@ -8,26 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /*
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://api.ipma.pt/open-data/distrits-islands.json')// envia GET request ao URL
-        .then(response => response.json()) // Põe a responsta no formato json
-        .then(data => console.log(data));
-});
-*/
-
-/*
+ */
 
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1100900.json')// envia GET request ao URL
+    let url = 'https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1100900.json';
+    fetch(url)// envia GET request ao URL
         .then(response => response.json()) // Põe a responsta no formato json
         .then(data => {
-            const tMax = data.tMax;
-            console-log(tMax);
-            for (let tem)
-            document.getElementById('Moeda').innerHTML = `Temperatura Maxima = ${tMax} `; // põe os dados na consola
-    }); // põe os dados na consola
+                console.log(data);
+                const country = data.country;
+                const tmax = data.data[0].tMax;
+                const tmin = data.data[0].tMin;
+                console.log(country);
+                console.log(tmax);
+        document.getElementById('Moeda').innerHTML = `Temperatura - Máxima: ${tmax} - Minima: ${tmin} `; // põe os dados na consola
+    });
 });
 
+
+/*
 document.addEventListener('DOMContentLoaded', () => {
 
     fetch('http://api.exchangeratesapi.io/v1/latest?access_key=feeea4a36c99c7c5f31c70dbfae3d215')
